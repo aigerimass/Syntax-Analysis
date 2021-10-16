937e79e4f9e75c76743ac1f04160fb72f9e4d23a
aigerimass <assylkhanova0@gmail.com>
2021-10-16 21:56:35 +0300

    all classes and some prints

--- parse_classes.py
+++ parse_classes.py
@@ -9,3 +9,376 @@ class Function:
         self.args = args
         self.body = body
         self.ret_value = ret_value
+
+
+class OpSkip:
+    def __init__(self):
+        pass
+
+
+class OpIf:
+    def __init__(self, condition, body, body_else):
+        self.condition = condition
+        self.body = body
+        self.body_else = body_else
+
+    def show(self):
+        print("\nIf(")
+        print("cond:")
+        self.condition.show()
+        print("\nbody:")
+        for op in self.body:
+            op.show()
+            print(";")
+        if self.body_else:
+            print("else body:")
+            for op in self.body:
+                op.show()
+                print(";")
+        print(")", end="")
+
+
+class OpWhile:
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+    def show(self):
+        print("While(\ncond: ")
+        self.condition.show()
+        print("\nbody:")
+        for op in self.body:
+            op.show()
+            print(";")
+        print(")", end="")
+
+
+class OpBinding:
+    def __init__(self, variable, expr):
+        self.variable = variable
+        self.expr = expr
+
+    def show(self):
+        print("Bind(", end="")
+        self.variable.show()
+        print(", ", end="")
+        self.expr.show()
+        print(")", end="")
+
+
+class OpFuncCall:
+    def __init__(self, name, args):
+        self.name = name
+        self.args = args
+
+    def show(self):
+        print("Call(name: \"", self.name, "\", args: ", end="", sep="")
+        for arg in self.args[:-1]:
+            arg.show()
+            print(", ", end="")
+        self.args[-1].show()
+        print(")", end="")
+
+
+class OpFuncReturn:
+    def __init__(self, expr):
+        self.expr = expr
+
+    def show(self):
+        print("Return(", end="")
+        self.expr.show()
+        print(")", end="")
+
+
+class Exp:  # E
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class ExpOr:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Or(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpWithoutOr:  # W
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class ExpAnd:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("And(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpWithoutAnd:  # V
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class ExpNot:
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        print("Not(", end="")
+        self.arg.show()
+        print(")", end="")
+
+
+class ExpWithoutNot:  # U
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class ExpEqual:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Eq(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpNotEqual:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("NotEq(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpLess:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Lt(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpLessEqual:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Leq(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpGreater:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Gt(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpGreaterEqual:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Geq(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpWithoutCompare:  # S
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class ExpPlus:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Plus(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpMinus:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+
+class ExpMonomial:  # M without + and -
+    def __init__(self, arg):
+        self.arg = arg
+
+
+class ExpMultiply:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+
+
+class ExpDivision:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Div(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+
+class ExpIndivisible:  # T
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class ExpUnaryMinus:
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        print("UnaryMinus(", end="")
+        self.arg.show()
+        print(")", end="")
+
+
+class ExpWithoutUnaryMinus:  # P
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class ExpPower:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def show(self):
+        print("Pow(", end="")
+        self.arg1.show()
+        print(", ", end="")
+        self.arg2.show()
+        print(")", end="")
+
+class ExpUnit:  # A
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        self.arg.show()
+
+
+class Number:
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        print(self.arg, end="")
+
+
+class StringLiteral:
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        print("\"", self.arg, "\"", end="", sep="")
+
+
+class ExpInBrackets:
+    def __init__(self, arg):
+        self.arg = arg
+
+    def show(self):
+        print("(", end="")
+        self.arg.show()
+        print(")", end="")
+
+
+class Variable:
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        print("Var \"", self.name, "\"", end="", sep="")
+
+
+t = ExpWithoutOr( ExpPower(ExpPlus(Number(3), StringLiteral("abcd")), Variable("tru")))
+t.show()
+print()
+g = OpFuncCall("namefunc", [Variable("arg1"), Variable("arg2")])
+g.show()
+print()
+r = OpFuncReturn(t)
+r.show()
+print()
+w = OpWhile(t, [g, r])
+w.show()
+i = OpIf(t, [w], [g])
+i.show()
+
